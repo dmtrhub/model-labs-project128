2d7b5e10387e485a469a2c4fe3f8983816ecbd72
dmtrhub <dmtr.develop@gmail.com>
2026-02-14 14:21:02 +0100

    Add project files.

---
+++ CIMProfileCreator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FTN.ESI.SIMES.CIM.CIMProfileCreator
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new CIMProfileCreatorForm());
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(string.Format("An error occurred.\nApplication is going down.\n\n{0}", e.Message), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
